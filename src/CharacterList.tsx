2bff7ba8653aea24dcf02efd34e70e58cab6e41b
Yuki Abe <12268087+yukia3@users.noreply.github.com>
2020-09-22 19:02:58 +0900

    Add Sample Module #3

---
+++ src/CharacterList.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from 'react';
+import { Header, Icon, Item } from 'semantic-ui-react';
+
+export type Character = {
+  id: number;
+  name: string;
+  grade: number;
+  height?: number;
+};
+
+type Props = {
+  school: string;
+  characters: Character[];
+};
+
+const CharacterList: FC<Props> = (props) => {
+  const { school, characters } = props;
+
+  return (
+    <>
+      <Header as="h2">{school}</Header>
+      <Item.Group>
+        {characters.map((character) => (
+          <Item key={character.id}>
+            <Icon name="user circle" size="huge" />
+            <Item.Content>
+              <Item.Header>{character.name}</Item.Header>
+              <Item.Meta>{character.grade}年生</Item.Meta>
+              <Item.Meta>
+                {character.height ?? character.height}
+                cm
+              </Item.Meta>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </>
+  );
+};
+
+export default CharacterList;
